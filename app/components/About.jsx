02aeb2cfbb4c3dd7fb9641421fd0af360d1fff1f
Guess Who <D:\The Bat!>
2016-10-19 16:55:45 +0300

    Lorem ipsum & style about

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 var About = (props) => {
 	return(
 		<div>
-			<h3>About</h3>
-			<p>Welcome to about page!</p>
+			<h3 className="text-center">About</h3>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat eos deserunt molestiae consequuntur veritatis animi necessitatibus veniam unde aliquam architecto libero aspernatur provident dolore temporibus, excepturi omnis fugit quo dicta!</p>
 		</div>
 	);
 };
