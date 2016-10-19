16b242318b4d46e3fa757957898148f51018e3de
Guess Who <D:\The Bat!>
2016-10-19 11:16:11 +0300

    Rename about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 var About = (props) => {
 	return(
-		<h3>About</h3>
-		<p>Welcome to about page!</p>
+		<div>
+			<h3>About</h3>
+			<p>Welcome to about page!</p>
+		</div>
 	);
 };
 
